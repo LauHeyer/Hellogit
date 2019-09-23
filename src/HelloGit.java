6b40686611f00a77807bfd8b8f38241d0f233e16
lhl <1220374970@qq.com>
2019-09-23 22:41:30 +0800

    testgit

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -3,5 +3,6 @@ public class HelloGit {
     public static void main(String[] args){
         System.out.println("helloGit,第一次上传....");
         System.out.println("That is ok....");
+        System.out.println("嗯....");
     }
 }
